f654d749ce5c721963dc85507b58ea4b710c3a81
Dan Engle <engle.68@gmail.com>
2024-04-15 17:45:53 -0500

    First version of neovim version notifier onload

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 require("mine.core")
 require("mine.lazy")
+
+local noice = require("noice")
+
+local ver = vim.inspect(vim.version())
+local minor = vim.inspect(vim.version()["minor"])
+local patch = vim.inspect(vim.version()["patch"])
+
+local smushed = minor .. "." .. patch
+noice.notify(ver, vim.log.levels.INFO, {
+  title = "Vim Version",
+  icon = "",
+})
+noice.notify(smushed, vim.log.levels.INFO, {
+  title = "Vim Version",
+  icon = "",
+})
