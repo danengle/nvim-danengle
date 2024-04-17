35020cb3b30c4d581a3f83eed9f1645013c6d06b
Dan Engle <engle.68@gmail.com>
2024-04-16 22:02:51 -0500

    Add mini and mini.animate

---
+++ lua/mine/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+  },
+  {
+    "echasnovski/mini.animate",
+    version = false,
+    dependencies = {
+      "echasnovski/mini.nvim",
+    },
+    config = true,
+  },
+}
