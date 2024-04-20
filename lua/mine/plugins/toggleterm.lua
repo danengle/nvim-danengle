a3de2864d8326ccddd9002adbde8faa6f6b0e1de
Dan Engle <engle.68@gmail.com>
2024-04-20 00:07:32 -0500

    Add toggleterm

---
+++ lua/mine/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    local term = require("toggleterm")
+    term.setup({
+      open_mapping = [[<A-\>]],
+    })
+    -- These don't seem to work as expected yet...open_mapping keymap works though
+    local topts = { buffer = 0 }
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], topts)
+    vim.keymap.set("t", "jk", [[<C-\><C-n>]], topts)
+    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], topts)
+    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], topts)
+    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], topts)
+    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], topts)
+    vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], topts)
+  end,
+}
