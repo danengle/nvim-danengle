52ae6861d91c59e6985a56f4acf2cd5af8e7f455
Dan Engle <engle.68@gmail.com>
2024-04-25 17:06:24 -0500

    Comment out version popup on startup

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 require("mine.core")
 require("mine.lazy")
 
-local noice = require("noice")
-
-local ver = vim.inspect(vim.version())
-local minor = vim.inspect(vim.version()["minor"])
-local patch = vim.inspect(vim.version()["patch"])
-
-local smushed = minor .. "." .. patch
-noice.notify(ver, vim.log.levels.INFO, {
-  title = "Vim Version",
-  icon = "",
-})
-noice.notify(smushed, vim.log.levels.INFO, {
-  title = "Vim Version",
-  icon = "",
-})
+-- local noice = require("noice")
+--
+-- local ver = vim.inspect(vim.version())
+-- local minor = vim.inspect(vim.version()["minor"])
+-- local patch = vim.inspect(vim.version()["patch"])
+--
+-- local smushed = minor .. "." .. patch
+-- noice.notify(ver, vim.log.levels.INFO, {
+--   title = "Vim Version",
+--   icon = "",
+-- })
+-- noice.notify(smushed, vim.log.levels.INFO, {
+--   title = "Vim Version",
+--   icon = "",
+-- })
 
 -- Astro has this snippet...use it to figure out best way to
 -- notify above and if nvim version even really matters...
