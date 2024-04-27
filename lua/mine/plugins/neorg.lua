25d502f414da41ab28363d6bf1e4430f178ae69d
Dan Engle <engle.68@gmail.com>
2024-04-27 12:44:27 -0500

    Add Neorg plugin

---
+++ lua/mine/plugins/neorg.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    version = "*",
+    lazy = false,
+    keys = {
+      { "<leader>ni", "<cmd>Neorg index<CR>", { desc = "Go to Neorg index", silent = true } },
+      { "<leader>nr", "<cmd>Neorg return<CRj", { desc = "Return from Neorg", silent = true } },
+    },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      })
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
+}
