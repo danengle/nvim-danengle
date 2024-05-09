bdd8e3e913f0c7fc871ee20cacd950d08b95135c
Dan Engle <engle.68@gmail.com>
2024-05-09 18:52:24 -0500

    Disable barbar and add Tabby rename tab mapping func

--- lua/mine/plugins/tabby.lua
+++ lua/mine/plugins/tabby.lua
@@ -3,6 +3,22 @@ return {
     "nanozuki/tabby.nvim",
     event = "VimEnter",
     dependencies = "nvim-tree/nvim-web-devicons",
+    keys = {
+      {
+        "<leader>tr",
+        function()
+          local tabby = require("tabby")
+          local tab_name = vim.fn.input("Rename this tab: ")
+          if not tab_name or tab_name == "" then
+            local notify = require("notify")
+            notify("Unable to rename tab with blank input", vim.log.levels.ERROR, { title = "Tabby:" })
+          else
+            tabby.tab_rename(tab_name)
+          end
+        end,
+        desc = "Rename current tab",
+      },
+    },
     config = function()
       require("tabby.tabline").use_preset("active_wins_at_tail", {
         theme = {
