af052c6195ef3ff6fe907f947a412e3da5633325
Dan Engle <engle.68@gmail.com>
2024-05-30 17:26:13 -0500

    Experimenting with Huez colorscheme picker and look at Neofusion

---
+++ lua/mine/plugins/themes/huez.lua
@@ -0,0 +1,11 @@
+if true then
+  return {}
+end
+-- return {
+--   "vague2k/huez.nvim",
+--   -- if you want registry related features, uncomment this
+--   import = "huez-manager.import",
+--   config = function()
+--     require("huez").setup({})
+--   end,
+-- }
